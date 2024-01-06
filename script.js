d362d07b79b6ff39d5ef01959770ddf01654c9a7
Ozodbek <devanabuildmuber@gmail.com>
2024-01-06 19:54:41 +0500

    this is my JavaScript 1 Lesson

---
+++ script.js
@@ -0,0 +1,66 @@
+//alert(5)
+
+/*
+let number = 13
+alert(number)
+*/
+
+/*
+const name = "Devana"
+alert(name)
+*/
+
+/*
+let maxSpeed = 13
+console.log(maxSpeed)
+*/
+
+// Numbers:
+let length = 16;
+let weight = 7.5;
+alert(length)
+console.log(weight)
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+alert(color)
+console.log(lastName)
+
+// Booleans
+let x = true;
+let y = false;
+alert(x)
+console.log(y)
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+alert(person)
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25"); 
+
+let d = "Gorilla" + 7 + 6
+alert(d)
+
+// Single quote inside double quotes:
+let answer1 = "It's alright";
+
+// Single quotes inside double quotes:
+let answer2 = "He is called 'Johnny'";
+
+// Double quotes inside single quotes:
+let answer3 = 'He is called "Johnny"';
+
+
+
+
+
+
+
+
+
+
